6b05a284f48717b7dc475b01b7060f2b3e69ec69
Jurn Wubben <46420489+jsw08@users.noreply.github.com>
2024-11-19 12:30:18 +0000

    Refactored code. TODO: events.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-// Require the necessary discord.js classes
 import { join } from "@std/path/join";
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 import config from "../config.json" with { type: "json" };
-import { SlashCommand } from "./types.d.ts";
+import setupCommands, { SlashCommand } from "./commands.ts"
+
+declare module "discord.js" {
+  export interface Client {
+    slashCommands: Collection<string, SlashCommand>;
+  }
+}
+
 
 const client = new Client({ intents: [] });
 
 client.slashCommands = new Collection<string, SlashCommand>();
-const handlersDir = join(import.meta.dirname!, "./handlers");
-for (const i of Deno.readDirSync(handlersDir)) {
-	if (!i.isFile || !i.name.endsWith(".ts")) break;
-	const module: { default: (client: Client) => void } = await import(
-		`file:///${handlersDir}/${i.name}`
-	);
-	module.default(client);
-}
+setupCommands(client)
 
-client.once(Events.ClientReady, (readyClient) => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+
 client.login(config.token);
+
