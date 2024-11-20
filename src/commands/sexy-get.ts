21d063a7f2f99a69b34703d2168195846f2bcd5a
Jurn Wubben <jurnwubben@gmail.com>
2024-11-20 18:06:57 +0100

    Almost finished get-sexy motherfucker command

--- src/commands/sexy-get.ts
+++ src/commands/sexy-get.ts
@@ -1,11 +1,15 @@
 import { ActionRowBuilder, EmbedBuilder, ButtonBuilder, SlashCommandBuilder, ButtonInteraction } from "discord.js";
 import { SlashCommand } from "../commandLoader.ts";
 import { ButtonStyle } from "discord.js";
+import { AutocompleteInteraction } from "discord.js";
+import { join } from "@std/path/join";
+import { Colors } from "discord.js";
+import { AttachmentBuilder } from "discord.js";
 
 const button = new ButtonBuilder()
     .setStyle(ButtonStyle.Danger)
     .setLabel("Delete")
-    .setCustomId("ping_delete")
+    .setCustomId("sexy-get_delete")
 const row = new ActionRowBuilder<ButtonBuilder>()
     .addComponents(button)
 
@@ -18,22 +22,66 @@ const command: SlashCommand = {
 	.setName("nickname") 
 	.setDescription("The nickname of the sexy mf.")
 	.setAutocomplete(true)
+	.setRequired(true)
   )
   ,
-  execute: (interaction) => {
+  execute: async (interaction) => {
+    const fileTypes = [".jpg", ".jpeg", ".png", ".webp", ".gif"]
+    const images: {name: string, file: AttachmentBuilder}[] = [];
+    const nickname = interaction.options.getString("nickname");
+
+    try {
+      for await (const file of Deno.readDir(join(import.meta.dirname!, `../../sexy-mfs/${nickname}/`))) {
+	const suffix = fileTypes.find(v => file.name.endsWith(v))
+	if (!file.isFile || !suffix) return;
+
+	const name = file.name.split(".")[0]
+	const attachement = new AttachmentBuilder(join(import.meta.dirname!, `../../sexy-mfs/${nickname}/${file.name}`), {name: name})
+	
+	images.push({name, file: attachement});
+      }
+    } catch (e) {
+      if (!(e instanceof Deno.errors.NotFound)) throw e;
+      
+      interaction.reply({
+	embeds: [new EmbedBuilder()
+	  .setColor(Colors.Red)	 
+	  .setDescription("That sexy motherfucker was not found :/")
+          .setFooter({text: "Brought to you by jsw's slaafje."}),
+	],
+	ephemeral: true
+      })
+      return
+    }
+
     interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setAuthor({ name: "jsw's slaafje" })
-          .setDescription(
-            `Pong! ${interaction.client.ws.ping}`,
-          ),
+      embeds: [new EmbedBuilder()
+	.setImage(`attachement://${images[0].name}`)
+	.setTitle(images[0].name)
       ],
+      files: [images[0].file],
       components: [row]
     });
+	//   interaction.reply({
+	//     embeds: images.map(v => (new EmbedBuilder()
+	//.setImage(v.image)
+	//.setTitle(v.name)
+	//     )),
+	//     components: [row]
+	//   });
   },
   button: (interaction: ButtonInteraction) => {
     interaction.deleteReply()
+  },
+  autocomplete: async (interaction: AutocompleteInteraction) => {
+    const userNames: string[] = [];
+
+    for await (const user of Deno.readDir(join(import.meta.dirname!, "../../sexy-mfs"))) {
+      if (!user.isDirectory) continue;
+      userNames.push(user.name)
+    }
+    
+    interaction.respond(userNames.map(v => ({name: v, value: v})))
   }
 };
 
