42995e2ba62699aea61a2f0ec4d3c931d9eeb503
Jurn Wubben <jurnwubben@gmail.com>
2024-11-20 21:34:17 +0100

    Implemented the sending part of sexy mfs. Still have to add the adding part (cnd api), set permissions and fix public mode.

--- src/commands/sexy-get.ts
+++ src/commands/sexy-get.ts
@@ -1,88 +1,74 @@
-import { ActionRowBuilder, EmbedBuilder, ButtonBuilder, SlashCommandBuilder, ButtonInteraction } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 import { SlashCommand } from "../commandLoader.ts";
 import { ButtonStyle } from "discord.js";
 import { AutocompleteInteraction } from "discord.js";
-import { join } from "@std/path/join";
 import { Colors } from "discord.js";
-import { AttachmentBuilder } from "discord.js";
+import importSexymfs from "../../sexy-mfs.json" with { type: "json" };
+
+const sexymfs = importSexymfs as { [x: string]: { [x: string]: string } };
 
 const button = new ButtonBuilder()
-    .setStyle(ButtonStyle.Danger)
-    .setLabel("Delete")
-    .setCustomId("sexy-get_delete")
+  .setStyle(ButtonStyle.Danger)
+  .setLabel("Delete")
+  .setCustomId("sexy-get_delete");
 const row = new ActionRowBuilder<ButtonBuilder>()
-    .addComponents(button)
+  .addComponents(button);
 
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName("sexy-get")
     .setDescription("Grabs a sexy mother fucking picture from the server's fs.")
-    .addStringOption(opt =>
+    .addStringOption((opt) =>
       opt
-	.setName("nickname") 
-	.setDescription("The nickname of the sexy mf.")
-	.setAutocomplete(true)
-	.setRequired(true)
-  )
-  ,
-  execute: async (interaction) => {
-    const fileTypes = [".jpg", ".jpeg", ".png", ".webp", ".gif"]
-    const images: {name: string, file: AttachmentBuilder}[] = [];
+        .setName("nickname")
+        .setDescription("The nickname of the sexy mf.")
+        .setAutocomplete(true)
+        .setRequired(true)
+    )
+    .addBooleanOption(opt => 
+      opt
+	.setName("public")
+	.setDescription("This option makes the response visible to everyone, disabled by default.")
+    ),
+  execute: (interaction) => {
     const nickname = interaction.options.getString("nickname");
-
-    try {
-      for await (const file of Deno.readDir(join(import.meta.dirname!, `../../sexy-mfs/${nickname}/`))) {
-	const suffix = fileTypes.find(v => file.name.endsWith(v))
-	if (!file.isFile || !suffix) return;
-
-	const name = file.name.split(".")[0]
-	const attachement = new AttachmentBuilder(join(import.meta.dirname!, `../../sexy-mfs/${nickname}/${file.name}`), {name: name})
-	
-	images.push({name, file: attachement});
-      }
-    } catch (e) {
-      if (!(e instanceof Deno.errors.NotFound)) throw e;
-      
+    if (!nickname || !sexymfs[nickname]) {
       interaction.reply({
-	embeds: [new EmbedBuilder()
-	  .setColor(Colors.Red)	 
-	  .setDescription("That sexy motherfucker was not found :/")
-          .setFooter({text: "Brought to you by jsw's slaafje."}),
-	],
-	ephemeral: true
-      })
-      return
+        embeds: [
+          new EmbedBuilder()
+            .setColor(Colors.Red)
+            .setDescription("That sexy motherfucker was not found :/")
+            .setFooter({ text: "Brought to you by jsw's slaafje." }),
+        ],
+        ephemeral: true,
+      });
+      return;
     }
 
+    const sexymf = sexymfs[nickname];
     interaction.reply({
-      embeds: [new EmbedBuilder()
-	.setImage(`attachement://${images[0].name}`)
-	.setTitle(images[0].name)
-      ],
-      files: [images[0].file],
-      components: [row]
+      embeds: Object.keys(sexymf).map((v) => (new EmbedBuilder()
+        .setImage(sexymf[v])
+        .setTitle(v))
+      ),
+      components: [row],
+      ephemeral: interaction.options.getBoolean("public") ?? true
     });
-	//   interaction.reply({
-	//     embeds: images.map(v => (new EmbedBuilder()
-	//.setImage(v.image)
-	//.setTitle(v.name)
-	//     )),
-	//     components: [row]
-	//   });
   },
   button: (interaction: ButtonInteraction) => {
-    interaction.deleteReply()
+    interaction.deleteReply();
   },
-  autocomplete: async (interaction: AutocompleteInteraction) => {
-    const userNames: string[] = [];
-
-    for await (const user of Deno.readDir(join(import.meta.dirname!, "../../sexy-mfs"))) {
-      if (!user.isDirectory) continue;
-      userNames.push(user.name)
-    }
-    
-    interaction.respond(userNames.map(v => ({name: v, value: v})))
-  }
+  autocomplete: (interaction: AutocompleteInteraction) => {
+    interaction.respond(
+      Object.keys(sexymfs).map((v) => ({ name: v, value: v })),
+    );
+  },
 };
 
 export default command;
