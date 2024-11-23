ab525406cab4fb417c7d02d1c5cfe9eca3524ab2
Jurn Wubben <jurnwubben@gmail.com>
2024-11-23 22:09:12 +0100

    Almost implemented sexy-get with a webserver. Still like to add a port for the web server in config and a paginator for the sexy-get.

--- src/commands/sexy-get.ts
+++ src/commands/sexy-get.ts
@@ -2,17 +2,17 @@ import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonInteraction,
-  EmbedBuilder,
+  InteractionResponse,
   SlashCommandBuilder,
 } from "discord.js";
 import { SlashCommand } from "../commandLoader.ts";
 import { ButtonStyle } from "discord.js";
 import { AutocompleteInteraction } from "discord.js";
-import { Colors } from "discord.js";
-import importSexymfs from "../../sexy-mfs.json" with { type: "json" };
 import { embed } from "../utils/embed.ts";
+import config from "../../config.json" with { type: "json" };
+import { join } from "@std/path/join";
+import { ChatInputCommandInteraction } from "discord.js";
 
-const sexymfs = importSexymfs as { [x: string]: { [x: string]: string } };
 
 const button = new ButtonBuilder()
   .setStyle(ButtonStyle.Danger)
@@ -20,6 +20,16 @@ const button = new ButtonBuilder()
   .setCustomId("sexy-get_delete");
 const row = new ActionRowBuilder<ButtonBuilder>()
   .addComponents(button);
+
+const sexyMfWasntFoundEmbed = (
+  interaction: ChatInputCommandInteraction,
+): Promise<InteractionResponse> =>
+  interaction.reply({
+    embeds: [
+      embed({ message: "That sexy mf wasn't found :/", kindOfEmbed: "error" }),
+    ],
+    ephemeral: true,
+  });
 
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
@@ -32,39 +42,92 @@ const command: SlashCommand = {
         .setAutocomplete(true)
         .setRequired(true)
     )
-    .addBooleanOption(opt => 
+    .addBooleanOption((opt) =>
       opt
-	.setName("public")
-	.setDescription("This option makes the response visible to everyone, disabled by default.")
+        .setName("public")
+        .setDescription(
+          "This option makes the response visible to everyone, disabled by default.",
+        )
     ),
-  execute: (interaction) => {
+  execute: async (interaction) => {
     const nickname = interaction.options.getString("nickname");
-    const revEphmeral = interaction.options.getBoolean( "public" )
-    if (!nickname || !sexymfs[nickname]) {
-      interaction.reply({
-        embeds: [ embed({message: "That sexy mf wasn't found :/", kindOfEmbed: "error"}) ],
-        ephemeral: true,
-      });
+    const revEphmeral = interaction.options.getBoolean("public");
+
+    if (!nickname || nickname === "NOSEXY") {
+      sexyMfWasntFoundEmbed(interaction);
       return;
     }
 
-    const sexymf = sexymfs[nickname];
-    interaction.reply({
-      embeds: Object.keys(sexymf).map((v) => embed({title: v, kindOfEmbed: "success"})
-        .setImage(sexymf[v])
-      ),
-      components: [row],
-      ephemeral: revEphmeral == null ? true : !revEphmeral
-    });
+    const images: string[][] = [[]];
+    try {
+      const sexyImageFiles = Deno.readDir(
+        join(import.meta.dirname!, "../../", config["sexy-mfs"].dir, nickname),
+      );
+      for await (const image of sexyImageFiles) {
+	if (images[images.length - 1].length === 10) images.push([]);
+	images[images.length - 1].push(image.name);
+      }
+    } catch (e) {
+      if (!(e instanceof Deno.errors.NotFound)) {
+        console.error(e);
+        interaction.reply({
+          embeds: [embed({
+            message: "Something went wrong :/",
+            kindOfEmbed: "error",
+          })],
+        });
+        return;
+      }
+
+      sexyMfWasntFoundEmbed(interaction);
+      return;
+    }
+    
+    for (const imageSubset of images) { // #TODO: Add forward and backward buttons for next pages, as this won't work (unknown interaction).
+      interaction.reply({
+	embeds: imageSubset.map((v) =>
+	  embed({ title: nickname.at(0)?.toUpperCase() + nickname.slice(1), kindOfEmbed: "success" })
+	    .setImage(new URL(`/${nickname}/${v}`, config["sexy-mfs"].image_url).toString())
+	    .setURL(config["sexy-mfs"].title_url)
+	),
+	components: [row],
+	ephemeral: revEphmeral == null ? true : !revEphmeral,
+      });
+    }
   },
   button: (interaction: ButtonInteraction) => {
     interaction.deleteReply();
   },
-  autocomplete: (interaction: AutocompleteInteraction) => {
+  autocomplete: async (interaction: AutocompleteInteraction) => {
+    const files = Deno.readDir(
+      join(import.meta.dirname!, "../../", config["sexy-mfs"].dir),
+    );
+    const sexymfs: { name: string; value: string }[] = [];
+    for await (const sexymf of files) sexymfs.push({ name: sexymf.name, value: sexymf.name });
+    
+    if (sexymfs.length === 0) {
+      sexymfs.push({
+        name: "There are currently no sexy motherfuckers available",
+        value: "NOSEXY",
+      });
+    }
+
     interaction.respond(
-      Object.keys(sexymfs).map((v) => ({ name: v, value: v })),
+      sexymfs,
     );
   },
 };
 
+
+
 export default command;
+
+import { serveDir } from "@std/http/file-server";
+
+Deno.serve((req) => {
+  return serveDir(req, {
+    fsRoot: `${config["sexy-mfs"].dir}`,
+  });
+});
+
+
