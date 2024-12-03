d04165df7901924e075b91112c3b36affbbdccd4
Jurn Wubben <46420489+jsw08@users.noreply.github.com>
2024-12-03 08:07:54 +0000

    minified code and implemented multiline eval

--- src/commands/utility/eval.ts
+++ src/commands/utility/eval.ts
@@ -1,10 +1,17 @@
 import {
+ActionRowBuilder,
   ButtonInteraction,
   InteractionReplyOptions,
+  ModalActionRowComponentBuilder,
+  ModalBuilder,
   SlashCommandBuilder,
+  TextInputBuilder,
+  TextInputStyle,
 } from "discord.js";
 import { SlashCommand } from "$/commandLoader.ts";
 import { embed } from "$utils/embed.ts";
+import { SlashCommandSubcommandBuilder } from "discord.js";
+import { Options } from "discord.js";
 
 const codeReplyOptions = (
   input: string,
@@ -20,61 +27,76 @@ const codeReplyOptions = (
       \`\`\`\n
       # Ouput
       \`\`\`ts\n${
-	output
-	  .map((e) => `${Deno.inspect(e, { compact: false, depth: 2 })}`)
-	  .join("\n")
-      }\`\`\``,
+      output
+        .map((e) => `${Deno.inspect(e, { compact: false, depth: 2 })}`)
+        .join("\n")
+    }\`\`\``,
   })],
 });
 const codeHandler = async (
-  code?: string,
+  code: string | null,
   ephemeral?: boolean,
 ): Promise<InteractionReplyOptions> => {
-    const results: string[] = [];
-    const ephemeralObject = ephemeral ? {ephemeral: true} : {}
+  const results: string[] = [];
+  const ephemeralObject = ephemeral ? { ephemeral: true } : {};
 
-    if (!code) {
-      return {
-        embeds: [embed({
-          kindOfEmbed: "error",
-          title: "Error!",
-          message: "Please provide a code string.",
-        })],
-	...ephemeralObject
-      };
-    }
+  if (!code) {
+    return {
+      embeds: [embed({
+        kindOfEmbed: "error",
+        title: "Error!",
+        message: "Please provide a code string.",
+      })],
+      ...ephemeralObject,
+    };
+  }
 
-    try {
-      if (code.includes("await")) {
-        results.push(
-          await eval(`(async () => {
+  try {
+    if (code.includes("await")) {
+      results.push(
+        await eval(`(async () => {
 	      ${code.replace("console.log", "results.push")}
 	  })()`),
-        );
-      } else {
-        results.push(await eval(code.replace("console.log", "results.push")));
-      }
-
-      return {
-        ...(codeReplyOptions(code, results)),
-	...ephemeralObject
-      }
-    } catch (e) {
-      const err = e as Error;
-      return {
-        embeds: [embed({
-          title: "Error!",
-          message: `\`\`\`js\n${err.name}: ${err.message}\nLine: ${
-            err.stack
-              ? err.stack.match(/<anonymous>:\d:\d/)?.[0].match(/\d:\d/)?.[0]
-              : ""
-          }\`\`\``,
-          kindOfEmbed: "error",
-        })],
-	...ephemeralObject
-      };
+      );
+    } else {
+      results.push(await eval(code.replace("console.log", "results.push")));
     }
-}
+
+    return {
+      ...(codeReplyOptions(code, results)),
+      ...ephemeralObject,
+    };
+  } catch (e) {
+    const err = e as Error;
+    return {
+      embeds: [embed({
+        title: "Error!",
+        message: `\`\`\`js\n${err.name}: ${err.message}\nLine: ${
+          err.stack
+            ? err.stack.match(/<anonymous>:\d:\d/)?.[0].match(/\d:\d/)?.[0]
+            : ""
+        }\`\`\``,
+        kindOfEmbed: "error",
+      })],
+      ...ephemeralObject,
+    };
+  }
+};
+
+const codeModal = () => {
+  const codeInput = new TextInputBuilder()
+    .setCustomId("code")
+    .setLabel("Typescript code.")
+    .setStyle(TextInputStyle.Paragraph);
+
+  const inputRow = new ActionRowBuilder<ModalActionRowComponentBuilder>()
+    .addComponents(codeInput);
+
+  return new ModalBuilder()
+    .setTitle("Typescript editor.")
+    .setCustomId(`${command.command.name}`)
+    .addComponents(inputRow);
+};
 
 const command: SlashCommand = {
   inDm: true,
@@ -83,70 +105,40 @@ const command: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName("ts")
     .setDescription("Runs the provided javascript code.")
-    .addSubcommand(subc => subc
-      .setName("inline")
-      .setDescription("For your beautiful ts-oneliners.")
-      .addStringOption((opts) =>
-	opts
-	  .setName("code")
-	  .setDescription("Write your javascript code here.")
-	  .setRequired(true)
-      ),
+    .addSubcommand((subc) =>
+      subc
+        .setName("inline")
+        .setDescription("For your beautiful ts-oneliners.")
+        .addStringOption((opts) =>
+          opts
+            .setName("code")
+            .setDescription("Write your javascript code here.")
+            .setRequired(true)
+        )
     )
-    .addSubcommand(subc => subc
-      .setName("multiline")
-      .setDescription("For your longer codepieces.")
-    ) 
-  ,
+    .addSubcommand((subc) =>
+      subc
+        .setName("multiline")
+        .setDescription("For your longer codepieces.")
+    ),
   execute: async (interaction) => {
     const code = interaction.options.getString("code");
-    const results: string[] = [];
+    const subc = interaction.options.getSubcommand(true)
 
-    if (!code) {
-      interaction.reply({
-        embeds: [embed({
-          kindOfEmbed: "error",
-          title: "Error!",
-          message: "Please provide a code string.",
-        })],
-      });
-      return;
+    if (subc === "multiline") {
+      interaction.showModal(codeModal())
+      return
     }
 
-    try {
-      if (code.includes("await")) {
-        results.push(
-          await eval(`(async () => {
-	      ${code.replace("console.log", "results.push")}
-	  })()`),
-        );
-      } else {
-        results.push(eval(code.replace("console.log", "results.push")));
-      }
-
-      await interaction.reply({
-        ...(codeReplyOptions(code, results)),
-        ephemeral: true,
-      });
-    } catch (e) {
-      const err = e as Error;
-      await interaction.reply({
-        embeds: [embed({
-          title: "Error!",
-          message: `\`\`\`js\n${err.name}: ${err.message}\nLine: ${
-            err.stack
-              ? err.stack.match(/<anonymous>:\d:\d/)?.[0].match(/\d:\d/)?.[0]
-              : ""
-          }\`\`\``,
-          kindOfEmbed: "error",
-        })],
-      });
-    }
+    interaction.deferReply()
+    await codeHandler(code, interaction);
   },
+  modal: async interaction => {
+    interaction.deferReply();
 
-  button: (interaction: ButtonInteraction) => {
-    interaction.deleteReply();
-  },
+    const code = interaction.fields.getField("code")
+    await codeHandler(code.value, interaction)
+  }
 };
 
 export default command;
