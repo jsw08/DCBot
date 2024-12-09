daa4f63b3d5ff423e25831d9a24937a9c894759f
Jurn Wubben <jurnwubben@gmail.com>
2024-12-10 00:03:11 +0100

    Formatting

--- src/commands/utility/reminder.ts
+++ src/commands/utility/reminder.ts
@@ -1,6 +1,4 @@
-import {
-  SlashCommandBuilder,
-} from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "$/commandLoader.ts";
 import { embed } from "$utils/embed.ts";
 import db from "$utils/db.ts";
@@ -17,25 +15,25 @@ db.sql`
 `;
 
 const sendReminders = () => {
-  const reminders = db.sql`SELECT * FROM reminders` 
+  const reminders = db.sql`SELECT * FROM reminders`;
   for (const i of reminders) {
     const reminder = i as {
-      id: number,
-      discord_id: string,
-      date: string,
-      message: string
-    } 
+      id: number;
+      discord_id: string;
+      date: string;
+      message: string;
+    };
     client.users.send(reminder.discord_id, {
-      embeds: [ embed({
-	title: "Reminding something for you :)",
-	message: `You told me to remember '${reminder.message}'!`,
-      })]
-    })
+      embeds: [embed({
+        title: "Reminding something for you :)",
+        message: `You told me to remember '${reminder.message}'!`,
+      })],
+    });
     db.exec("DELETE FROM reminders WHERE id = :id", { id: reminder.id });
   }
-}
-sendReminders()
-setInterval(sendReminders, parseInt(config.REMINDER_TIMEOUT))
+};
+sendReminders();
+setInterval(sendReminders, parseInt(config.REMINDER_TIMEOUT));
 
 const command: SlashCommand = {
   inDm: true,
@@ -44,14 +42,34 @@ const command: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName("remind")
     .setDescription("Sets a reminder.")
-    .addStringOption(opts => opts
-      .setName("message")
-      .setDescription("The message you're sending yourself.")
-      .setRequired(true)
-    )
-  ,
+    .addStringOption((opts) =>
+      opts
+        .setName("message")
+        .setDescription("The message you're sending yourself.")
+        .setRequired(true)
+    ),
   execute: (interaction) => {
-    db.exec("INSERT INTO reminders (discord_id, date, message) VALUES (:discord_id, :date, :message)", { discord_id: interaction.user.id, date: "", message: interaction.options.getString("message", true)});
+    let cont: boolean = true
+    interaction.user.send("test").then((v) => v.delete()).catch((v) => {
+      interaction.reply({
+        embeds: [embed({
+          title: "DM Error",
+          message: "I'm unable to send you a direct message.",
+          kindOfEmbed: "error",
+        })],
+      });
+      cont = false;
+    });
+    if (cont) return
+
+    db.exec(
+      "INSERT INTO reminders (discord_id, date, message) VALUES (:discord_id, :date, :message)",
+      {
+        discord_id: interaction.user.id,
+        date: "",
+        message: interaction.options.getString("message", true),
+      },
+    );
   },
 };
 
