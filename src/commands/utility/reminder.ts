54e9133d17a38ee1228ca7f064cba9b8e6a14185
Jurn Wubben <jurnwubben@gmail.com>
2024-12-11 01:49:00 +0100

    Not working but added database date shit for confirm button and fixed dm check

--- src/commands/utility/reminder.ts
+++ src/commands/utility/reminder.ts
@@ -1,30 +1,33 @@
-import { SlashCommandBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "$/commandLoader.ts";
 import { embed } from "$utils/embed.ts";
 import db from "$utils/db.ts";
 import { config } from "$utils/config.ts";
 import { client } from "$/main.ts";
-import { DiscordAPIError } from "discord.js";
+import { ButtonStyle } from "discord.js";
 
 db.sql`
   CREATE TABLE IF NOT EXISTS reminders (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     discord_id TEXT NOT NULL,
-    date TEXT NOT NULL,
     message text not null,
-    message_id text not null
+    date TEXT NOT NULL,
+    send_date TEXT NOT NULL,
+    confirmed INTEGER NOT NULL
   )
 `;
+type Reminder = {
+  id: number,
+  discord_id: string;
+  message: string;
+  date: string;
+  send_date: string;
+  confirmed: number;
+}
 
 const sendReminders = () => {
-  const reminders = db.sql`SELECT * FROM reminders`;
-  for (const i of reminders) {
-    const reminder = i as {
-      id: number;
-      discord_id: string;
-      date: string;
-      message: string;
-    };
+  const reminders = db.sql`SELECT message, id, discord_id, date FROM reminders WHERE confirmed = 1 AND date < unixepoch('now')`.forEach(i => {
+    const reminder = i as Reminder;
     client.users.send(reminder.discord_id, {
       embeds: [embed({
         title: "Reminding something for you :)",
@@ -32,7 +35,9 @@ const sendReminders = () => {
       })],
     });
     db.exec("DELETE FROM reminders WHERE id = :id", { id: reminder.id });
-  }
+  })
+  const q = db.sql`DELETE FROM reminders WHERE confirmed = 0 AND send_date < unixepoch('now', '-2 seconds');`
+  !q.length || console.log(q)
 };
 sendReminders();
 setInterval(sendReminders, parseInt(config.REMINDER_TIMEOUT));
@@ -51,17 +56,34 @@ const command: SlashCommand = {
         .setRequired(true)
     ),
   execute: async (interaction) => {
+    if (!Object.keys(interaction.authorizingIntegrationOwners).includes("1")) {
+      interaction.reply({
+	embeds: [embed({
+	  title: "Reminder ERROR",
+	  message: `Please install this bot into your account. Else I won't be able to send a DM to you.`,
+	  kindOfEmbed: "error",
+	})],
+	components: [new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder()
+	  .setLabel("Install")
+	  .setURL(`https://discord.com/oauth2/authorize?client_id=${config.DC_CLIENT_ID}`)
+	  .setEmoji("1267495233990688828")
+	  .setStyle(ButtonStyle.Link)
+	)]	
+      })
+      return
+    }
+
     const message = interaction.options.getString("message", true);
     db.exec(
-      "INSERT INTO reminders (discord_id, date, message) VALUES (:discord_id, :date, :message)",
+      "INSERT INTO reminders (discord_id, date, message, send_date, confirmed) VALUES (:discord_id, :date, :message, :send_date, :confirmed)",
       {
 	discord_id: interaction.user.id,
-	date: "",
+	date: new Date(),
 	message: message,
+	send_date: Date.now(),
+	confirmed: true
       },
     );
-
-    interaction.user.createDM();
   },
 };
 
